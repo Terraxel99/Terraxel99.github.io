c39651246725af550053f59c5d697c3a834bea2b
oruckaplanulb <oruc.kaplan@ulb.be>
2022-11-19 14:01:36 +0100

    Orthogonal preliminar version

---
+++ js/orthogonal.js
@@ -0,0 +1,373 @@
+/* eslint-disable no-undef, no-unused-vars */
+
+/* This class represents a point. */
+class Point {
+    constructor(x, y, nb, color, adjList) {
+        this.x = x;
+        this.y = y;
+        this.number = nb;
+        this.color = color; // 0 = black, 1 = red, 2 = green, 3 = blue
+        this.adjList = adjList;
+    }
+}
+
+/* This class represents an edge. */
+class Edge {
+    constructor(pt1, pt2, color) {
+        this.pt1 = pt1;
+        this.pt2 = pt2;
+        this.color = color;
+    }
+}
+
+/* This class represents an ear. */
+class Ear {
+    constructor(pt1, pt2, pt3, pt4) {
+        this.pt1 = pt1;
+        this.pt2 = pt2;
+        this.pt3 = pt3;
+        this.pt4 = pt4;
+    }
+}
+
+// global variables
+var points = [];
+var pointsForComputation = [];
+var edges = [];
+var quadrilateralizationEdges = [];
+var isPolygonCreated = false;
+var occurenceOfEachColor = [0, 0, 0, 0, 0];
+const colors = ["black", "red", "green", "blue", "yellow", "grey"];
+
+function setup() {
+    const canvasContainer = document.getElementById("illustration1-canvas");
+    let canvas = createCanvas(canvasContainer.offsetWidth, 450);
+    canvas.parent("illustration1-canvas");
+    canvas.mousePressed(addPoint);
+
+    fill("black");
+    textSize(40);
+    button = createButton("Clear");
+    button.position(10, 10);
+    button.style("z-index", "3");
+    button.parent("illustration1-canvas");
+    button.mouseReleased(resetpoints);
+
+    button2 = createButton("Create polygon");
+    button2.position(70, 10);
+    button2.style("z-index", "3");
+    button2.parent("illustration1-canvas");
+    button2.mouseReleased(createPolygon);
+
+    button3 = createButton("Apply Chvátal's watchman theorem");
+    button3.position(195, 10);
+    button3.style("z-index", "3");
+    button3.parent("illustration1-canvas");
+    button3.mouseReleased(applyQuadrilateralization);
+}
+
+/* This method applies the triangulation on the drawn polygon */
+function applyQuadrilateralization() {
+    if (isPolygonCreated) {
+        reccursiveQuadrilateralization(points, quadrilateralizationEdges);
+    } else {
+        document.getElementById("illustation1-result").innerHTML =
+            "The polygon is not created yet";
+    }
+}
+
+/* This function clear the list of points, the polygon and the result computed */
+function resetpoints() {
+    points = [];
+    edges = [];
+    quadrilateralizationEdges = [];
+    isPolygonCreated = false;
+    document.getElementById("illustation1-result").innerHTML = "";
+}
+
+function createPolygon() {
+    isPolygonCreated = true;
+    //add the last edge connecting edge 0 with the last one to close the polygon.
+
+    points[0].adjList.push(points[points.length - 1]);
+    points[points.length - 1].adjList.push(points[0]);
+    edges.push(new Edge(points[0], points[points.length - 1], 0));
+}
+
+/* This function add a point to the list of points and call the function 
+of the exercice if needed */
+function addPoint() {
+    if (!isPolygonCreated) {
+        if (points.length === 0) {
+            points.push(new Point(mouseX, mouseY, 0, 0, []));
+        } else if (points.length === 1) {
+            if (Math.abs(mouseX - points[0].x) > Math.abs(mouseY - points[0].y)) {
+                points.push(new Point(mouseX, points[0].y, points[points.length - 1].number + 1, 0, []));
+            } else {
+                points.push(new Point(points[0].x, mouseY, points[points.length - 1].number + 1, 0, []));
+            }
+        } else {
+            if (points[points.length - 2].x === points[points.length - 1].x) {
+                points.push(new Point(mouseX, points[points.length - 1].y, points[points.length - 1].number + 1, 0, []));
+            } else if (points[points.length - 2].y === points[points.length - 1].y) {
+                points.push(new Point(points[points.length - 1].x, mouseY, points[points.length - 1].number + 1, 0, []));
+            }
+        }
+        if (points.length > 1) {
+            points[points.length - 2].adjList.push(points[points.length - 1]);
+            points[points.length - 1].adjList.push(points[points.length - 2]);
+            edges.push(
+                new Edge(points[points.length - 2], points[points.length - 1], 0)
+            );
+        }
+    }
+}
+
+/*This function check if a point is inside using the 3 determinant given 
+in parameter */
+function isInside(det1, det2, det3) {
+    return (
+        (det1 >= 0 && det2 >= 0 && det3 >= 0) ||
+        (det1 <= 0 && det2 <= 0 && det3 <= 0)
+    );
+}
+
+/*This function compute the determinant of the 3 points given in parameter */
+function computeDeterminant(pt1, pt2, pt3) {
+    let a = pt2.x - pt1.x;
+    let b = pt3.x - pt1.x;
+    let c = pt2.y - pt1.y;
+    let d = pt3.y - pt1.y;
+    let det = a * d - b * c;
+    return det;
+}
+
+/* This function sorts the points by Y-coordinates */
+function sortByYCoordinate() {
+    points.sort(compare);
+}
+
+/* This is the comparaison function used to sort the points by Y-coordinate */
+function compare(a, b) {
+    if (a.y - b.y === 0) {
+        return a.x - b.x;
+    } else {
+        return a.y - b.y;
+    }
+}
+
+/* This function checks if the point i (of the global variable points) 
+is convex (if it is left turn since we check in counter clock wise) */
+function isConvex(i, ptsList) {
+    let det = 0;
+    if (i === 0) {
+        det = computeDeterminant(
+            ptsList[ptsList.length - 1],
+            ptsList[i],
+            ptsList[i + 1]
+        );
+    } else if (i === ptsList.length - 1) {
+        det = computeDeterminant(ptsList[i - 1], ptsList[i], ptsList[0]);
+    } else {
+        det = computeDeterminant(ptsList[i - 1], ptsList[i], ptsList[i + 1]);
+    }
+    return det < 0;
+}
+
+/* This function checks if there is no concave points 
+inside the square formed by the points i and his 3 followers */
+function noConcaveInside(i, ptsList) {
+    let currEar = get3Following(i, ptsList);
+    let result1 = false;
+    let result2 = false;
+    for (let j = 0; j < ptsList.length; j++) {
+        if (
+            ptsList[j].number !== currEar.pt1.number &&
+            ptsList[j].number !== currEar.pt2.number &&
+            ptsList[j].number !== currEar.pt3.number &&
+            ptsList[j].number !== currEar.pt4.number
+        ) {
+            // This part checks if ptsList[j] is inside the triangle pt1, pt2, pt3
+            let det1 = computeDeterminant(currEar.pt1, currEar.pt2, ptsList[j]);
+            let det2 = computeDeterminant(currEar.pt2, currEar.pt3, ptsList[j]);
+            let det3 = computeDeterminant(currEar.pt3, currEar.pt1, ptsList[j]);
+            result1 = isInside(det1, det2, det3);
+            det1 = computeDeterminant(currEar.pt1, currEar.pt3, ptsList[j]);
+            det2 = computeDeterminant(currEar.pt3, currEar.pt4, ptsList[j]);
+            det3 = computeDeterminant(currEar.pt4, currEar.pt1, ptsList[j]);
+            result2 = isInside(det1, det2, det3);
+            if (result1 || result2) {
+                break;
+            }
+        }
+    }
+    return !result1 && !result2;
+}
+
+function get3Following(i, pts) {
+    let tmpEar;
+    switch (i) {
+        case pts.length - 1:
+            tmpEar = new Ear(pts[pts.length - 1], pts[0], pts[1], pts[2]);
+            break;
+        case pts.length - 2:
+            tmpEar = new Ear(pts[pts.length - 2], pts[pts.length - 1], pts[0], pts[1]);
+            break;
+        case pts.length - 3:
+            tmpEar = new Ear(pts[pts.length - 3], pts[pts.length - 2], pts[pts.length - 1], pts[0]);
+            break;
+        default:
+            tmpEar = new Ear(pts[i], pts[i + 1], pts[i + 2], pts[i + 3]);
+    }
+    return tmpEar;
+}
+
+/* This function returns an ear of the polygon. */
+function findEar(ptsList) {
+    let ear;
+    let i = 0;
+    let earNotFound = true;
+    while (earNotFound) {
+        if (isConvex(i + 1, ptsList) && isConvex(i + 2, ptsList)) {
+            if (noConcaveInside(i, ptsList)) {
+                earNotFound = false;
+                ear = get3Following(i, ptsList);
+                //colorEar(ear);
+            }
+        }
+        if (earNotFound) {
+            i++;
+        }
+    }
+    return [ear, i];
+}
+
+/**
+ * This method tricolors the graph in O(n) time.
+ */
+function triColorGraph() {
+    points[0].color = 1;
+    points[1].color = 2;
+    for (let i = 1; i < points.length - 1; i++) {
+        if (points[i].adjList.length % 2 !== 0) {
+            points[i + 1].color = points[i - 1].color;
+        } else {
+            points[i + 1].color = 6 - points[i - 1].color - points[i].color;
+        }
+        occurenceOfEachColor[points[i + 1].color]++;
+    }
+}
+
+/**
+ * This method returns the number of the color of the guards and display in the html the result.
+ * @returns the number of the color of the guards.
+ */
+function findGuardsPosition() {
+    let min = Infinity;
+    let colorGuard = 0;
+    for (let i = 1; i < occurenceOfEachColor.length; i++) {
+        if (occurenceOfEachColor[i] < min) colorGuard = i;
+    }
+
+    //document.getElementById("illustation1-result").innerHTML =
+    //    "The color of the guards is " + colors[colorGuard] + ".";
+    return colorGuard;
+}
+
+/* This method apply the quadrilateralization on the polygon reccursively.
+The method searches after an ear of the polygon and when it finds one,
+ it "removes" it and the algo is restarted on the "new" polygon. 
+ It ends when the polygon has only 3 vertices left 
+ (and therefore this one is itself a square). */
+function reccursiveQuadrilateralization(ptsList, egsList) {
+    if (ptsList.length > 4) {
+        let result = findEar(ptsList);
+        let ear = result[0];
+        let newptsList = [...ptsList];
+        newptsList.splice(result[1] + 1, 2);
+        //closing the square
+        points[ear.pt1.number].adjList.push(points[ear.pt4.number]);
+        points[ear.pt4.number].adjList.push(points[ear.pt1.number]);
+        //adding the two diagonals
+        points[ear.pt1.number].adjList.push(points[ear.pt3.number]);
+        points[ear.pt3.number].adjList.push(points[ear.pt1.number]);
+        points[ear.pt4.number].adjList.push(points[ear.pt2.number]);
+        points[ear.pt2.number].adjList.push(points[ear.pt4.number]);
+        //adding the 3 new edges
+        egsList.push(new Edge(ear.pt1, ear.pt4, 0));
+        egsList.push(new Edge(ear.pt1, ear.pt3, 5));
+        egsList.push(new Edge(ear.pt2, ear.pt4, 5));
+        reccursiveQuadrilateralization(newptsList, egsList);
+    } else if (ptsList.length === 4) {
+        points[ptsList[0].number].adjList.push(points[ptsList[2].number]);
+        points[ptsList[2].number].adjList.push(points[ptsList[0].number]);
+        points[ptsList[1].number].adjList.push(points[ptsList[3].number]);
+        points[ptsList[3].number].adjList.push(points[ptsList[1].number]);
+        egsList.push(new Edge(ptsList[0], ptsList[2], 5));
+        egsList.push(new Edge(ptsList[1], ptsList[3], 5));
+        triColorGraph();
+        findGuardsPosition();
+    }
+}
+
+function draw() {
+    // Put drawings here
+    background(200);
+
+    //This part draws the points and lines of the polygon
+    let c;
+    for (let i = 0; i < points.length; i++) {
+        c = color(colors[points[i].color]);
+        fill(c);
+        noStroke();
+        textSize(14);
+        text(points[i].number, points[i].x + 5, points[i].y + 5);
+        ellipse(points[i].x, points[i].y, 7, 7);
+    }
+    for (let i = 0; i < edges.length; i++) {
+        c = color(colors[edges[i].color]);
+        stroke(c);
+        fill(c);
+        line(edges[i].pt1.x, edges[i].pt1.y, edges[i].pt2.x, edges[i].pt2.y);
+    }
+
+    // This part draws the lines create for the quadrilateralization.
+    for (let i = 0; i < quadrilateralizationEdges.length; i++) {
+        c = color(colors[quadrilateralizationEdges[i].color]);
+        stroke(c);
+        line(
+            quadrilateralizationEdges[i].pt1.x,
+            quadrilateralizationEdges[i].pt1.y,
+            quadrilateralizationEdges[i].pt2.x,
+            quadrilateralizationEdges[i].pt2.y
+        );
+    }
+    stroke("black");
+}
+
+// This Redraws the Canvas when resized
+windowResized = function () {
+    resize();
+};
+
+/** Toggles the modal of the illustration */
+function toggleModal() {
+    const modal = document.getElementById("illustration1-modal");
+
+    if (modal.style.display === "none" || modal.style.display === "") {
+        modal.style.display = "block";
+        resize();
+    } else {
+        modal.style.display = "none";
+    }
+}
+
+function resize() {
+    const canvasContainer = document.getElementById("illustration1-canvas");
+    resizeCanvas(canvasContainer.offsetWidth, 450);
+}
+
+const toggles = document.querySelectorAll(".illustration1-toggle");
+toggles.forEach((toggle) => {
+    toggle.addEventListener("click", toggleModal);
+});
